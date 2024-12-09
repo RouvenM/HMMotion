e3873e4708024368018e1e0a605330d40daeb632
Rouven Michels <michels@fb05.statistik.uni-dortmund.de>
2024-12-09 23:11:50 +0100

    .

--- plots.R
+++ plots.R
@@ -95,3 +95,64 @@ for (t in unique(tracking_kc_az$time)) {
   Sys.sleep(0.1) # Wartezeit in Sekunden
 }
 
+
+# Feste Farbskala (von hellgrau zu schwarz)
+color_palette <- colorRampPalette(c("lightgray", "black"))
+
+# Anzahl an Farbstufen definieren (z. B. 100)
+n_colors <- 100
+colors <- color_palette(n_colors)# Animation: Spielerbewegungen
+for (t in unique(tracking_kc_az$time)) {
+  # Daten für den aktuellen Zeitpunkt filtern
+  current_data <- tracking_kc_az[tracking_kc_az$time == t, ]
+  
+  # Spielfeld zeichnen
+  plot(
+    NA, 
+    xlim = c(min(tracking_kc_az$x)-1, max(tracking_kc_az$x)+1),#c(0, field_length), 
+    ylim = c(min(tracking_kc_az$y)-1, max(tracking_kc_az$y)+1),#c(0, field_width),
+    xlab = "Spielfeld-Länge", ylab = "Spielfeld-Breite",
+    main = paste("Spielerbewegungen - Zeit:", t), asp = 1
+  )
+  grid() # Raster für das Spielfeld
+  
+  # Spielerpositionen hinzufügen
+  points(current_data$x, current_data$y, pch = 19, 
+         col = team_colors[as.factor(current_data$club)], cex = 2)
+  abline(v = los)
+  #text(current_data$x, current_data$y, labels = current_data$nflId, pos = 3, col = "black")
+  
+  # Verbindungslinien basierend auf Wahrscheinlichkeiten hinzufügen
+  defender_probs <- defender_data[defender_data$time == t, ]#[[t %% length(defender_data) + 1]] # Beispiel: Wahrscheinlichkeiten für diesen Zeitpunkt
+  for (i in 1:nrow(defender_probs)) {
+    defender <- defender_probs$defender_Id[i]
+    # Maximale Wahrscheinlichkeit für diesen Verteidiger
+    #max_prob <- which.max(defender_probs[i, 20:24])
+    for (j in 1:n_att) {
+      attacker <- as.numeric(names(defender_probs)[j + 19])
+      current_data2 = current_data %>% filter(!is.na(nflId))
+      # Koordinaten von Verteidiger und Angreifer finden
+      defender_pos <- current_data[which(current_data2$nflId == defender), c("x", "y")]
+      attacker_pos <- current_data[which(current_data2$nflId == attacker), c("x", "y")]
+      
+      # Mapping der y-Werte auf die Farbskala
+      y_scaled <- round(defender_probs[i,j+19] * (n_colors - 1)) + 1
+      mapped_colors <- colors[y_scaled]
+      # Linie zeichnen, falls beide gefunden wurden
+      #if (nrow(defender_pos) > 0 && nrow(attacker_pos) > 0) {
+        lines(
+          x = c(defender_pos$x, attacker_pos$x),
+          y = c(defender_pos$y, attacker_pos$y),
+          lwd = 3,
+          col = mapped_colors #, # Linienstärke proportional zu y[i]
+        )
+      #}      
+    }
+
+  }
+  
+  # Pause für Animation
+  Sys.sleep(0.1) # Wartezeit in Sekunden
+}
+
+
